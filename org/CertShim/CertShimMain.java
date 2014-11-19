4eb5941faf9bd78753fd5b29cf1e6696b1b7cc53
akatsuki521 <hanjiajun@ufl.edu>
2014-11-19 16:43:05 -0500

    Add content

--- org/CertShim/CertShimMain.java
+++ org/CertShim/CertShimMain.java
@@ -3,6 +3,17 @@ package org.CertShim;
 /**
  * The main frame of CertShim.
  */
-public class CertShimMain {
+import javax.net.ssl.SSLSocket;
+import java.net.Socket;
+import java.security.cert.CertificateException;
 
+
+public class CertShimMain{
+    static void check(Socket socket) throws CertificateException{
+        if(!socket.isConnected()||!(socket instanceof SSLSocket)){
+            System.out.println("Not a SSL connection. Checking not performed.");
+            return;
+        }
+        boolean converge=JConvergence.check();
+    }
 }
