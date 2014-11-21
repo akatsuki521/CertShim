484c996d55bda11611469aa38606896d721c0a67
akatsuki521 <hanjiajun@ufl.edu>
2014-11-20 21:07:50 -0500

    Add revocation.

--- org/CertShim/RevokeCheck.java
+++ org/CertShim/RevokeCheck.java
@@ -1,10 +1,26 @@
 package org.CertShim;
 
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSession;
+import java.security.cert.Certificate;
+import sun.security.provider.certpath.OCSP;
+
 /**
- * Created by hanjiajun on 11/20/14.
+ * Check if the target certificate is revoked.
  */
 public class RevokeCheck implements SSLCheckable {
-    public boolean check(String host, String port){
-        return true;
+    public boolean check(SSLSession session){
+        Certificate[] certs;
+        Certificate peerCert;
+        Certificate issuerCert;
+        /*TODO find a way to get issuer's certificate.*/
+        try {
+            certs = session.getPeerCertificates();
+            peerCert=certs[0];
+        }catch (SSLPeerUnverifiedException e){
+            System.out.println("Not a valid SSL connection");
+            return false;
+        }
+        return OCSP.check(peerCert, issuerCert);
     }
 }
