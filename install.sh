089194104bac00e05d73472ed13692dbf5719e23
akatsuki521 <hanjiajun@ufl.edu>
2014-12-07 14:31:17 -0500

    Fix file path bug

--- install.sh
+++ install.sh
@@ -30,6 +30,6 @@ javac -cp lib/*:. org/CertShim/*.java;
 jar cf libs.jar org/CertShim/CertShimMain.class org/CertShim/CheckThread.class org/CertShim/JConverge*.class org/CertShim/SSLCheckable.class
 mv libs.jar $HOME/.CertShim
 cp lib/*.jar $HOME/.CertShim
-jar cfm ag.jar man org/CertShim/Agent.class org/CertShim/CertShimTrans.class
+jar cfm ag.jar lib/man org/CertShim/Agent.class org/CertShim/CertShimTrans.class
 mv ag.jar $HOME/.CertShim
 
